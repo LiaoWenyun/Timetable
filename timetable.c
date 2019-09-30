dac6c23369d53748263345101e2069a9e0ab211f
wenyun <xiliao2@hotmail.com>
2019-09-30 10:38:56 -0700

    fixed format

--- timetable.c
+++ timetable.c
@@ -1,8 +1,12 @@
+//examples in config.txt file
+//ENG422(A)  Mon 9:30am-10:30am  Wed 3:00pm-4:30pm
+//SNEG422(B)  Mon 1:30pm-3:30pm 
+//CSC455(A)  Wed 10:30am-11:30am 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
 
 
 struct Class{
@@ -13,7 +17,6 @@ struct Class{
 };                 
 
 
-//declaration of the functions
 char* get_today();
 void yellow();
 void reset();
@@ -27,43 +30,56 @@ int schedule(struct Class*, struct Class*, struct Class*, struct Class*, struct 
 char* space(char*);
 void print_schedule(struct Class*, struct Class*, struct Class* , struct Class* , struct Class*);
 
-//examples in config.txt file
-//ENG422(A)  Mon 9:30am-10:30am  Wed 3:00pm-4:30pm
-//SNEG422(B)  Mon 1:30pm-3:30pm 
-//CSC455(A)  Wed 10:30am-11:30am 
 
+/**
+/* Gets the local current date 
+/* @param none 
+/* @return pointer to char string today, which will be Mon/Tus/Wed/Thu/Fri 
+**/
 char* get_today(){
   	char* rest;
 	time_t now;    // time_t is arithmetic time type
 	time(&now);    //get the current time 
 	char *today = strtok_r(ctime(&now)," ",&rest);	
 	return today;
-
 }
 
+
+/**
+/* Sets the charactors to be yellow when printing 
+/* @param none
+/* @return none
+**/
 void yellow(){
-		  //printf("\033[1;31m");       //red
-			printf("\033[0;33m");
+	printf("\033[0;33m");
 }
 
+
+/**
+/* Reset the charactor color
+/* @param none
+/* @return none
+**/
 void reset(){
-		  printf("\033[0m");
+	printf("\033[0m");
 }
 
+
+/**
+/* Open a file and parse the lines
+/* @param pointer to a Class array class_list
+/* @return none
+**/
 void read_file(struct Class* class_list){
-	
 	FILE *file; 
 	char buffer[150]; 								 	//buffer to store the line
 	char* path_to_file = getenv("HOME");				//get the environment variable which refer to the home directory  	
 	char* f_name =  "/.config/timetable/config.txt";
 	strcat( path_to_file , f_name );
 	file = fopen(path_to_file , "r");
-
-
 	if(file ==NULL){
 		printf("ERROR: no config file found");
 		exit(1);
-
 	}else{
 		int class_position = 0;
 		while(fgets(buffer, sizeof(buffer), file)){	   //iterate through the file line by line 	
@@ -92,21 +108,29 @@ void read_file(struct Class* class_list){
 				token = strtok_r(NULL, " ",&end_str);	
 				count++;
 			}
-		
 		}	
 	}
-
 }
 
+
+/**
+/* Function prints out the content of each element in a Class array 
+/* @param pointer to a Class array class_list 
+/* @return none
+**/
 void print_array(struct Class* class_list){
 	int i =0;
 	for(i=0; i<30;i++){
 		printf("node: %d, class name: %s, class day: %s , class time: %d, class length: %d \n ", i, class_list[i].class_name, class_list[i].day, class_list[i].start_time, class_list[i].length);
-		
 		}
-
 }
 
+
+/**
+/* parse the time from the lines 
+/* @param pointer to char token
+/* @return integer type
+**/
 int parse_time(char* token){
 	char* time;
 	time = strdup(token);
@@ -115,6 +139,12 @@ int parse_time(char* token){
 	return map_time(start_time);
 }
 
+
+/**
+/* calculate the length of each class 
+/* @param pointer to char token
+/* @return integer type 
+**/
 int parse_length(char *token){
 	char* time;
 	time = strdup(token);
@@ -123,74 +153,88 @@ int parse_length(char *token){
 	return map_time(end_time) - map_time(start_time);
 }
 
+
+/**
+/* map the time string to an integer 
+/* @param pointer to char time 
+/* @return integer type
+**/
 int map_time(char *time){
 	int result = 0;
 	if( strcmp(time,"8:30am") == 0){result = 0;}
 	else if( strcmp(time, "8:50am")==0){ result =20;}  
 	else if( strcmp(time, "9:00am")==0){ result =30;}
-   	else if( strcmp(time, "9:20am")==0){ result =50;} 
-    else if( strcmp(time, "9:30am")==0){ result =60;}			
-    else if( strcmp(time,"9:50am")==0){ result =80;}
-    else if( strcmp(time,"10:00am")==0){ result =90;}	
-    else if( strcmp(time,"10:20am")==0){ result =110;}					
-    else if( strcmp(time,"10:30am")==0){ result =120;}
-    else if( strcmp(time,"10:50am")==0){ result =140;}
-    else if( strcmp(time,"11:00am")==0){ result =150;}	
-    else if( strcmp(time,"11:20am")==0){ result =170;}				 		
-    else if( strcmp(time,"11:30am")==0){ result =180;}
-    else if( strcmp(time,"11:50am")==0){ result =200;}	
-    else if( strcmp(time,"12:00pm")==0){ result =210;}	
-    else if( strcmp(time,"12:20pm")==0){ result =230;}	
-    else if( strcmp(time,"12:30pm")==0){ result =240;}
-    else if( strcmp(time,"12:50pm")==0){ result =260;}	
-    else if( strcmp(time,"1:00pm")==0){ result =270;}
-    else if( strcmp(time,"1:20pm")==0){ result =290;}	
-    else if( strcmp(time,"1:30pm")==0){ result =300;}
-    else if( strcmp(time,"1:50pm")==0){ result =320;}
-    else if( strcmp(time,"2:00pm")==0){ result =330;}	
-    else if( strcmp(time,"2:20pm")==0){ result =350;}	
-    else if( strcmp(time,"2:30pm")==0){ result =360;}	
-    else if( strcmp(time,"2:50pm")==0){ result =380;}
-    else if( strcmp(time,"3:00pm")==0){ result =390;}	
-    else if( strcmp(time,"3:20pm")==0){ result =410;}	
-    else if( strcmp(time,"3:30pm")==0){ result =420;}
-    else if( strcmp(time,"3:50pm")==0){ result =440;}
-    else if( strcmp(time,"4:00pm")==0){ result =450;}	
-   	else if( strcmp(time,"4:20pm")==0){ result =470;}
+   	else if( strcmp(time, "9:20am")==0){ result =50;}
+	else if( strcmp(time, "9:30am")==0){ result =60;}
+	else if( strcmp(time,"9:50am")==0){ result =80;}
+	else if( strcmp(time,"10:00am")==0){ result =90;}
+	else if( strcmp(time,"10:20am")==0){ result =110;}					
+	else if( strcmp(time,"10:30am")==0){ result =120;}
+	else if( strcmp(time,"10:50am")==0){ result =140;}
+	else if( strcmp(time,"11:00am")==0){ result =150;}
+	else if( strcmp(time,"11:20am")==0){ result =170;}				 		
+	else if( strcmp(time,"11:30am")==0){ result =180;}
+	else if( strcmp(time,"11:50am")==0){ result =200;}	
+	else if( strcmp(time,"12:00pm")==0){ result =210;}	
+	else if( strcmp(time,"12:20pm")==0){ result =230;}	
+	else if( strcmp(time,"12:30pm")==0){ result =240;}
+	else if( strcmp(time,"12:50pm")==0){ result =260;}	
+	else if( strcmp(time,"1:00pm")==0){ result =270;}
+	else if( strcmp(time,"1:20pm")==0){ result =290;}	
+	else if( strcmp(time,"1:30pm")==0){ result =300;}
+	else if( strcmp(time,"1:50pm")==0){ result =320;}
+	else if( strcmp(time,"2:00pm")==0){ result =330;}	
+	else if( strcmp(time,"2:20pm")==0){ result =350;}	
+	else if( strcmp(time,"2:30pm")==0){ result =360;}	
+	else if( strcmp(time,"2:50pm")==0){ result =380;}
+	else if( strcmp(time,"3:00pm")==0){ result =390;}	
+	else if( strcmp(time,"3:20pm")==0){ result =410;}	
+	else if( strcmp(time,"3:30pm")==0){ result =420;}
+	else if( strcmp(time,"3:50pm")==0){ result =440;}
+	else if( strcmp(time,"4:00pm")==0){ result =450;}
+	else if( strcmp(time,"4:20pm")==0){ result =470;}
    	else if( strcmp(time,"4:30pm")==0){ result =480;}
    	else if( strcmp(time,"4:50pm")==0){ result =500;}
 	else if( strcmp(time,"5:00pm")==0){ result =510;}   
-    else if( strcmp(time,"5:20pm")==0){ result =530;}
-    else if( strcmp(time,"5:30pm")==0){ result =540;}
-    else if( strcmp(time,"5:50pm")==0){ result =560;}
-    else if( strcmp(time,"6:00pm")==0){ result =570;}
-    else if( strcmp(time,"6:20pm")==0){ result =590;}	
-    else if( strcmp(time,"6:30pm")==0){ result =600;}				
-    else if( strcmp(time,"6:50pm")==0){ result =620;}	
-    else if( strcmp(time,"7:00pm")==0){ result =630;}	
-    else if( strcmp(time,"7:20pm")==0){ result =650;}	
-    else if( strcmp(time,"7:30pm")==0){ result =660;}	
-    else if( strcmp(time,"7:50pm")==0){ result =680;}	
-    else if( strcmp(time,"8:00pm")==0){ result =690;}
-    else if( strcmp(time,"8:20pm")==0){ result =710;}	
-    else if( strcmp(time,"8:30pm")==0){ result =720;}	
-    else if( strcmp(time,"8:50pm")==0){ result =740;}	
-    else if( strcmp(time,"9:00pm")==0){ result =750;}	
-    else if( strcmp(time,"9:20pm")==0){ result =770;}	
-    else if( strcmp(time,"9:30pm")==0){ result =780;}
-    else if( strcmp(time,"9:50pm")==0){ result =800;}	
-    else if( strcmp(time,"10:00pm")==0){ result =810;}
-    else if( strcmp(time,"10:20pm")==0){ result =830;}
+	else if( strcmp(time,"5:20pm")==0){ result =530;}
+	else if( strcmp(time,"5:30pm")==0){ result =540;}
+	else if( strcmp(time,"5:50pm")==0){ result =560;}
+	else if( strcmp(time,"6:00pm")==0){ result =570;}
+	else if( strcmp(time,"6:20pm")==0){ result =590;}	
+	else if( strcmp(time,"6:30pm")==0){ result =600;}				
+	else if( strcmp(time,"6:50pm")==0){ result =620;}	
+	else if( strcmp(time,"7:00pm")==0){ result =630;}	
+	else if( strcmp(time,"7:20pm")==0){ result =650;}	
+	else if( strcmp(time,"7:30pm")==0){ result =660;}	
+	else if( strcmp(time,"7:50pm")==0){ result =680;}	
+	else if( strcmp(time,"8:00pm")==0){ result =690;}
+	else if( strcmp(time,"8:20pm")==0){ result =710;}	
+	else if( strcmp(time,"8:30pm")==0){ result =720;}	
+	else if( strcmp(time,"8:50pm")==0){ result =740;}	
+	else if( strcmp(time,"9:00pm")==0){ result =750;}	
+	else if( strcmp(time,"9:20pm")==0){ result =770;}	
+	else if( strcmp(time,"9:30pm")==0){ result =780;}
+	else if( strcmp(time,"9:50pm")==0){ result =800;}	
+	else if( strcmp(time,"10:00pm")==0){ result =810;}
+	else if( strcmp(time,"10:20pm")==0){ result =830;}
 	else if( strcmp(time,"10:30pm")==0){ result =840;}
 	else if( strcmp(time,"10:50pm")==0){ result =860;}	 
 	else if( strcmp(time,"11:00pm")==0){ result =870;}
 	else if( strcmp(time,"11:20pm")==0){ result =890;}	 
-    else if( strcmp(time,"11:30pm")==0){ result =900;}
-	else{  printf("no time found \n");}    
-
+	else if( strcmp(time,"11:30pm")==0){ result =900;}
+	else{  
+		printf("no time found \n");
+	}   
 	return result;
 }
 
+
+
+/**
+/* Scheduler, distribute the class node to appropriate array and arrage the array  
+/* @param pointer to a Class array class_list, M_class, T_class, W_class, Th_class, F_class
+/* @return integer type
+**/
 int schedule(struct Class* class_list, struct Class* M_class, struct Class* T_class, struct Class* W_class, struct Class* Th_class, struct Class* F_class){
 	int i ;
 	int conflict = 0;
@@ -234,13 +278,20 @@ int schedule(struct Class* class_list, struct Class* M_class, struct Class* T_cl
 						 }else{
 							F_class[class_list[i].start_time/30 + j] = class;
 						 }	
-				 }else{ return conflict; }		 
-
+				 }else{ return conflict;
+				      }		 
 			}
 	}
 	return conflict;
 }
 
+
+
+/**
+/* Insert empty classes into the five arrays
+/* @param pointer to a Class array M_class, T_class, W_class, Th_class, F_class
+/* @return none
+**/
 void insert_empty_class( struct Class* M_class, struct Class* T_class, struct Class* W_class, struct Class* Th_class, struct Class* F_class){
 	int i ;
 	int tmp_start_time = 0;
@@ -252,44 +303,45 @@ void insert_empty_class( struct Class* M_class, struct Class* T_class, struct Cl
 		Th_class[i] = class;
 		F_class[i] = class;
 	}
-
 }
 
+
+/**
+/* format the diaplayed class name to be 10 space
+/* @param pointer to a char array name
+/* @return pointer to a char array (with formated length)
+**/
 char* space(char* name){
-	
 	char* tmp_name;
 	if(strlen(name)==10){
 		return name;
 	}else if(strlen(name)==9){
 		tmp_name = " ";
 		strncat(name, tmp_name, 1); 		
-
 	}else if(strlen(name)==8){
 		tmp_name = "  ";
 		strncat(name,tmp_name, 1);
-
 	}else if(strlen(name)==7){
 		tmp_name ="   ";
 		strncat(name,tmp_name, 1);
-
 	}else if(strlen(name)==6){
 		tmp_name ="    ";
 		strncat(name,tmp_name, 1);
-		
 	}else{
-
 		return name;
 	}
 	return name;
 }
 
 
-
+/**
+/* Prints out the time table
+/* @param pointer to a Class array class_list, M_class, T_class, W_class, Th_class, F_class
+/* @return none
+**/
 void print_schedule(struct Class* M_class, struct Class* T_class, struct Class* W_class, struct Class* Th_class, struct Class* F_class){
-	
 	char* today = get_today();
-	printf(" ----------------------------------------------------------------------------------------------\n");	
-	//printf("|         |     Monday     |     Tuesday    |    Wednesday   |    Thursday    |     Friday     |\n",mon,tue,wed,thu,fri);	
+	printf(" ----------------------------------------------------------------------------------------------\n");		
 	printf("|         |" );
 	if(strcmp(today,"Mon")==0){
 		yellow();
@@ -331,7 +383,6 @@ void print_schedule(struct Class* M_class, struct Class* T_class, struct Class* 
 		printf("     Friday     ");
 	}
 	printf("|\n");
-
 	printf("|----------------------------------------------------------------------------------------------|\n");
 	int i;
 	for(i=0; i<30; i++){
@@ -347,15 +398,11 @@ void print_schedule(struct Class* M_class, struct Class* T_class, struct Class* 
 				unit = " pm";
 			}
 		}
-	
 		int min = ((i+1)%2)*3; 
-			
 		printf("|%d:%d0%s -|----------------|----------------|----------------|----------------|----------------|\n", hour,min,unit);
 		printf("|         |   %s   |   %s   |   %s   |   %s   |   %s   |\n",space(M_class[i].class_name),space(T_class[i].class_name),space(W_class[i].class_name),space(Th_class[i].class_name),space(F_class[i].class_name) );
-
 	}
 	printf(" ----------------------------------------------------------------------------------------------\n");
-
 }
 
 
@@ -368,19 +415,8 @@ int main(){
 	struct Class Th_class[37];
 	struct Class F_class[37];
 	read_file(class_list);          //parse the file, and atore all the Class structs into the class list 
-//	print_array(class_list);		//print out all the class struct insode the class struct
 	insert_empty_class(M_class, T_class, W_class, Th_class, F_class);
 	int conflict = schedule(class_list,M_class, T_class, W_class, Th_class, F_class); 
-	if(conflict ==0){print_schedule(M_class, T_class, W_class, Th_class, F_class);}		
+	if(conflict ==0){print_schedule(M_class, T_class, W_class, Th_class, F_class);}	
 	return 0;
-
 }
-
-
-
-
-
-
-
-      
-
